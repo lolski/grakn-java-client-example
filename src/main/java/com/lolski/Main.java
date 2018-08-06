62863117bbb6957cf675af08200cdf31a6c8fdf5
Ganeshwara Herawan Hananda <soulmagic13@gmail.com>
2018-08-06 11:06:10 +0100

    changed to parse()

--- src/main/java/com/lolski/Main.java
+++ src/main/java/com/lolski/Main.java
@@ -17,41 +17,33 @@ import static ai.grakn.graql.Graql.*;
 public class Main {
     public static void main(String[] args) {
         final String GRAKN_URI = "localhost:48555";
-        final String GRAKN_KEYSPACE = "grakn14";
+        final String GRAKN_KEYSPACE = "grakn";
         try (Grakn.Session session = Grakn.session(new SimpleURI(GRAKN_URI), Keyspace.of(GRAKN_KEYSPACE))) {
             System.out.println("defining the parent-child schema...");
             // define schema
             try (GraknTx tx = session.transaction(GraknTxType.WRITE)) {
-                tx.graql().define(
-                        label("name").sub("attribute").datatype(AttributeType.DataType.STRING),
-                        label("person").sub("entity").has("name").plays("parent").plays("child"),
-                        label("parentchild").sub("relationship").relates("parent").relates("child")
-                ).execute();
+                tx.graql().parse("define\n" +
+                        "        name sub attribute datatype string;\n" +
+                        "        person sub entity, has name, plays parent, plays child;\n" +
+                        "        parentchild sub relationship, relates parent, relates child;").execute();
                 tx.commit();
             }
 
             // define the parent, Johnny Sr.
             System.out.println("inserting parent & child...");
             try (GraknTx tx = session.transaction(GraknTxType.WRITE)) {
-                tx.graql().insert(var().isa("person").has("name").val("Johnny Jr.")).execute();
-                tx.graql().insert(var().isa("person").has("name").val("Johnny Sr.")).execute();
-                tx.graql().match(
-                        var("prnt").isa("person").has("name", "Johnny Sr."),
-                        var("chld").isa("person").has("name", "Johnny Jr."))
-                    .insert(var().isa("parentchild").rel("parent", "prnt").rel("child", "chld"))
-                    .execute();
+                tx.graql().parse("insert isa person has name \"Johnny Sr.\";").execute();
+                tx.graql().parse("insert isa person has name \"Johnny Jr.\";").execute();
+                tx.graql().parse("match $prnt isa person has name \"Johnny Sr.\"; $chld isa person has name \"Johnny Jr.\"; insert (parent: $prnt, child: $chld) isa parentchild;").execute();
                 tx.commit();
             }
 
             // print the created relationship
             System.out.println("print the created relationship, and perform aggregate count followed by compute count queries...");
             try (GraknTx tx = session.transaction(GraknTxType.READ)) {
-                List<ConceptMap> results = tx.graql().match(
-                        var("prnt").isa("person").has("name", "Johnny Sr."),
-                        var("chld").isa("person").has("name", "Johnny Jr."),
-                        var("prntchld").rel("parent", "prnt").rel("child", "chld"))
-                    .get().execute();
+                List<ConceptMap> results = (List<ConceptMap>) tx.graql().parse("match $prnt isa person has name \"Johnny Sr.\"; $chld isa person has name \"Johnny Jr.\"; $prntchld(parent: $prnt, child: $chld) isa parentchild; get;").execute();
                 for (ConceptMap result: results) {
+
                     System.out.println(result.get("prnt").id() + " name = \"Johnny Sr.\" (prnt) --> (chld) " +
                             result.get("chld").id() + " name = \"Johnny Jr.\" via relationship " + result.get("prntchld").id());
                 }
